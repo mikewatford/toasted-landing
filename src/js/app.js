f07a880a78ca3b13ebbbd74c7b45e20fc443d7d0
cje <cj_eustaquio@apple.com>
2014-09-10 13:52:56 -0700

    initial angular routes config

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,36 @@
-var toastedLife = angular.module( 'toastedLife', [] );
+var toastedLife = angular.module( 'toastedLife', [ 'ngRoute' ] );
+ 
+toastedLife.config( [ '$routeProvider', function( $routeProvider ){
+  $routeProvider
+    
+    // route for home page
+    .when( '/', {
+      templateUrl : 'pages/home.html',
+      controller  : 'mainController'
+    })
+
+    // route for about page
+    .when( '/about', {
+      templateUrl : 'pages/about.html',
+      controller : 'aboutController'
+    })
+
+    // route for contact page
+    .when( '/contact', {
+      templateUrl : 'pages/contact.html',
+      controller : 'contactController'
+    });
+
+}] );
 
 toastedLife.controller( 'mainController', [ '$scope', function($scope) {
   $scope.message = 'Shmoney';
+}] );
 
+toastedLife.controller( 'aboutController', [ '$scope', function($scope) {
+  $scope.message = 'All about that shmoney';
+}] );
+
+toastedLife.controller( 'contactController', [ '$scope', function($scope) {
+  $scope.message = 'Contact a a a';
 }] );
