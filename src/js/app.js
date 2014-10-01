037ec32d0fafa7c610efa80a9b6067f61a93a4f1
cje <cj_eustaquio@apple.com>
2014-10-01 16:44:10 -0700

    moving site to use skrollr to handle parallax behavior

--- src/js/app.js
+++ src/js/app.js
@@ -1,73 +1,7 @@
-var toastedLife = angular.module( 'toastedLife', [ 'ngRoute', 'ngAnimate' ] );
+// Init Skrollr
+var s = skrollr.init({
+  forceHeight: false
+});
  
-toastedLife.config( [ '$routeProvider', '$locationProvider', function( $routeProvider, $locationProvider ){
-  $routeProvider
-    
-    // route for home page
-    .when( '/', {
-      templateUrl : 'pages/home.html',
-      controller  : 'mainController'
-    })
-
-    // route for definition page
-    .when( '/2', {
-      templateUrl : 'pages/definition.html',
-      controller  : 'definitionController'
-    })
-
-    // route for mission page
-    .when( '/3', {
-      templateUrl : 'pages/mission.html',
-      controller  : 'missionController'
-    })
-
-    // route for gallery page
-    .when( '/4', {
-      templateUrl : 'pages/gallerylanding.html',
-      controller  : 'galleryController'
-    })
-
-    // route for blog page
-    .when( '/5', {
-      templateUrl : 'pages/bloglanding.html',
-      controller  : 'blogController'
-    });
-
-  // $locationProvider.html5Mode( true );
-
-}] );
-
-toastedLife.controller( 'mainController', [ '$scope', function($scope) {
-  $scope.message   = 'Shmoney';
-  $scope.pageClass = 'page-home flex-zone';
-  $scope.navPrev   = '/';
-  $scope.navNext   = '2';
-}] );
-
-toastedLife.controller( 'definitionController', [ '$scope', function($scope) {
-  $scope.message   = 'All about that shmoney';
-  $scope.pageClass = 'page-definition flex-zone';
-  $scope.navPrev   = '/';
-  $scope.navNext   = '3';
-}] );
-
-toastedLife.controller( 'missionController', [ '$scope', function($scope) {
-  $scope.message   = 'Contact a a a';
-  $scope.pageClass = 'page-mission flex-zone';
-  $scope.navPrev   = '2';
-  $scope.navNext   = '4';
-}] );
-
-toastedLife.controller( 'galleryController', [ '$scope', function($scope) {
-  $scope.message   = 'A week agoooooo';
-  $scope.pageClass = 'page-gallerylanding';
-  $scope.navPrev   = '3';
-  $scope.navNext   = '5';
-}] );
-
-toastedLife.controller( 'blogController', [ '$scope', function($scope) {
-  $scope.message   = 'Shmurda team';
-  $scope.pageClass = 'page-bloglanding';
-  $scope.navPrev   = '4';
-  $scope.navNext   = '/';
-}] );
+// Refresh Skrollr after resizing our sections
+s.refresh($('.homeSlide'));
